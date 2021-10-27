d0f720d71c1e3d0e1afc9bf3705a184399668bed
michael@dockter.com <michael@dockter.com>
2021-10-27 15:28:59 -0400

    #61 Update non-root install

--- bin/docker-images.sh
+++ bin/docker-images.sh
@@ -5,6 +5,7 @@
 export DOCKER_IMAGES=(
     "senzing/init-container:${SENZING_DOCKER_IMAGE_VERSION_INIT_CONTAINER:-latest}"
     "senzing/resolver:${SENZING_DOCKER_IMAGE_VERSION_RESOLVER:-latest}"
+    "senzing/senzing-base:${SENZING_DOCKER_IMAGE_VERSION_SENZING_BASE:-latest}"
     "senzing/senzing-console:${SENZING_DOCKER_IMAGE_VERSION_SENZING_CONSOLE:-latest}"
     "senzing/yum:${SENZING_DOCKER_IMAGE_VERSION_YUM:-latest}"
 )
