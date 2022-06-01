d1773c2a299e860fbc50381c2d57a2f18e45f2d7
docktermj <michael@dockter.com>
2022-06-01 14:34:42 -0400

    #84 Savepoint

--- resolver.py
+++ resolver.py
@@ -18,7 +18,6 @@ import signal
 import string
 import sys
 import time
-from distutils.util import strtobool
 from enum import IntFlag
 from urllib.parse import urlparse, urlunparse
 
@@ -1301,6 +1300,23 @@ def handle_post_resolver(iterator, senzing_engine_flags=None):
     logging.info(message_info(103, line_count, len(result)))
 
     return result
+
+
+def strtobool(val):
+    """ From /usr/lib/python3.8/distutils/util.py
+
+    Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
 
 # -----------------------------------------------------------------------------
 # Flask @app.routes
